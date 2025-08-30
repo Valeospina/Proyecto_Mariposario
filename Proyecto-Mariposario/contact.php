e83c8e93d6eadde7bfe202b85ca6ee5c5f53910b
Valeospina <123786911+Valeospina@users.noreply.github.com>
2025-08-30 11:12:17 -0600

    ubicacion

--- Proyecto-Mariposario/contact.php
+++ Proyecto-Mariposario/contact.php
@@ -64,7 +64,7 @@
 						<div class="col-lg-6">
 							<div class="contact-us-left">
 								<!--Start Google-map -->
-								<div id="myMap"></div>
+								<iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3927.3190515489305!2d-84.537853!3d10.1547!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTDCsDA5JzE2LjkiTiA4NMKwMzInMTYuMyJX!5e0!3m2!1ses-419!2scr!4v1756573897905!5m2!1ses-419!2scr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 								
 								<!--/End Google-map -->
 							</div>
